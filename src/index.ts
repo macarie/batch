d452a9841de138ed7fd318e882e50cc0d751eb89
Raul Macarie <raul@macarie.me>
2021-06-01 23:20:28 +0200

    Implement the `limit` option's behavior

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
+interface Options {
+  /**
+		Maximum number of calls within an `interval`.
+
+    @default Number.POSITIVE_INFINITY
+		*/
+  readonly limit?: number
+}
+
 /**
  * Batches multiple function-calls into one by creating a throttled function that only invokes `f` at most once per every `wait` milliseconds.
  * When the time comes, it invokes `f` with an array that contains the arguments of every function-call that did not run, grouped, as these are collected and batched.
  *
- * @param f The function to throttle that should be invoked with batched arguments.
- * @param wait The number of milliseconds to throttle invocations to.
- * @returns Returns the new throttled function.
+ * @param f The function you want to throttle.
+ * @param interval Timespan for `limit` in milliseconds.
+ * ```
  */
 const batch = <T extends unknown[]>(
   f: (parameters: T[]) => void,
-  wait = 0
+  interval = 0,
+  { limit = Number.POSITIVE_INFINITY }: Options = {}
 ): ((...parameters: T) => void) => {
-  if (typeof wait !== 'number' || wait < 0) {
+  if (typeof interval !== 'number' || interval < 0) {
     throw new TypeError('Expected the second argument to be a positive number')
   }
 
@@ -18,21 +28,39 @@ const batch = <T extends unknown[]>(
     throw new TypeError('Expected the first argument to be a function')
   }
 
-  let throttled = false
+  if (typeof limit !== 'number' || limit < 0) {
+    throw new TypeError('Expected the `limit` option to be a positive number')
+  }
+
+  let timesCalled = 0
   let args: T[] = []
+  let timeout: NodeJS.Timeout | undefined
 
   return (...parameters: T): void => {
+    timesCalled += 1
     args.push(parameters)
 
-    if (!throttled) {
-      throttled = true
-
-      setTimeout(() => {
+    if (timesCalled === 1) {
+      timeout = setTimeout(() => {
         f(args)
 
-        throttled = false
+        timesCalled = 0
         args = []
-      }, wait)
+        timeout = undefined
+      }, interval)
+    }
+
+    if (timesCalled > limit) {
+      f(args)
+
+      timesCalled = 0
+      args = []
+
+      if (timeout !== undefined) {
+        clearTimeout(timeout)
+
+        timeout = undefined
+      }
     }
   }
 }
