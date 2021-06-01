7fdbf54951711f53b55a116e669e2404d12975c6
Raul Macarie <raul@macarie.me>
2021-06-01 23:13:34 +0200

    Add tests for the `limit` option

--- tests/batch.ts
+++ tests/batch.ts
@@ -66,6 +66,56 @@ test('should batch calls every 30ms', async () => {
   ])
 })
 
+test('when the function is called more times than `limit` allows, it should run immediately', async () => {
+  let argumentsReceived: ArgumentsType[] = []
+
+  const testFunction = (args: ArgumentsType[]) => {
+    argumentsReceived = args
+  }
+
+  const batchedFunction = batch(testFunction, 30, { limit: 2 })
+
+  batchedFunction(1, 'a', true)
+  batchedFunction(2, 'b', false)
+
+  assert.equal(argumentsReceived, [])
+
+  batchedFunction(3, 'c', true)
+
+  assert.equal(argumentsReceived, [
+    [1, 'a', true],
+    [2, 'b', false],
+    [3, 'c', true],
+  ])
+})
+
+test('`limit` should work when it is set to 0', async () => {
+  let argumentsReceived: ArgumentsType[] = []
+
+  const testFunction = (args: ArgumentsType[]) => {
+    argumentsReceived = args
+  }
+
+  const batchedFunction = batch(testFunction, 30, { limit: 0 })
+
+  batchedFunction(1, 'a', true)
+
+  assert.equal(argumentsReceived, [[1, 'a', true]])
+
+  batchedFunction(2, 'b', false)
+
+  assert.equal(argumentsReceived, [[2, 'b', false]])
+
+  batchedFunction(3, 'c', true)
+
+  assert.equal(argumentsReceived, [[3, 'c', true]])
+
+  argumentsReceived = []
+
+  await delay(50)
+  assert.equal(argumentsReceived, [])
+})
+
 test('should throw if `f` is not a function', () => {
   assert.throws(
     () => batch({} as unknown as (parameters: unknown[][]) => void, 0),
@@ -75,7 +125,7 @@ test('should throw if `f` is not a function', () => {
   )
 })
 
-test('should throw if `wait` is not a number', () => {
+test('should throw if `interval` is not a number', () => {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   const testFunction = () => {}
 
@@ -87,7 +137,7 @@ test('should throw if `wait` is not a number', () => {
   )
 })
 
-test('should throw if `wait` is a negative number', () => {
+test('should throw if `interval` is a negative number', () => {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   const testFunction = () => {}
 
@@ -96,6 +146,30 @@ test('should throw if `wait` is a negative number', () => {
     (error) =>
       error instanceof TypeError &&
       error.message === 'Expected the second argument to be a positive number'
+  )
+})
+
+test('should throw if `limit` is not a number', () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const testFunction = () => {}
+
+  assert.throws(
+    () => batch(testFunction, 0, { limit: [] as unknown as number }),
+    (error) =>
+      error instanceof TypeError &&
+      error.message === 'Expected the `limit` option to be a positive number'
+  )
+})
+
+test('should throw if `limit` is a negative number', () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const testFunction = () => {}
+
+  assert.throws(
+    () => batch(testFunction, 0, { limit: -1 }),
+    (error) =>
+      error instanceof TypeError &&
+      error.message === 'Expected the `limit` option to be a positive number'
   )
 })
 
